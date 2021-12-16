02c3bf93b46c4fd09e4c32c9f0cd1d37b982a863
Zero Dev <akbarabdul80@gmail.com>
2021-12-16 09:48:03 +0700

    Add Anim

--- app/src/main/java/com/ugm/kaskita/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ugm/kaskita/ui/splash/SplashActivity.kt
@@ -3,16 +3,33 @@ package com.ugm.kaskita.ui.splash
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.oratakashi.viewbinding.core.binding.activity.viewBinding
 import com.oratakashi.viewbinding.core.tools.startActivity
 import com.ugm.kaskita.R
+import com.ugm.kaskita.databinding.ActivitySplashBinding
 import com.ugm.kaskita.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val appSpalsh: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.app_splash)
+    }
+
+    private val bottomToTop: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.bottom_totop)
+    }
+
+    private val binding: ActivitySplashBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        binding.appLogo.startAnimation(appSpalsh)
+        binding.tvNameApp.startAnimation(bottomToTop)
 
         Handler(Looper.getMainLooper()).postDelayed(
             {
