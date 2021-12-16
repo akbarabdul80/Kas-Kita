af0835a05837bc3e9cef028d2f5013c80affaf37
Zero Dev <akbarabdul80@gmail.com>
2021-12-16 09:49:38 +0700

    Fix Anim

--- app/src/main/java/com/ugm/kaskita/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ugm/kaskita/ui/splash/SplashActivity.kt
@@ -26,7 +26,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
 
         binding.appLogo.startAnimation(appSpalsh)
         binding.tvNameApp.startAnimation(bottomToTop)
